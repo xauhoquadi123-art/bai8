0ac607aef2570addc2244269d937a51339010118
xauhoquadi123-art <xauhoquadi123@gmail.com>
2026-03-18 13:43:35 +0700

    thuchanh2.js

---
+++ thuchanh1.js
@@ -0,0 +1,16 @@
+let nam = parseInt(prompt("enter a year"));
+if (nam % 4===0 ){
+   if (nam % 100===0){
+
+   }else {
+
+   }
+   if (nam % 400===0){
+       alert("năm nhuận");
+   }else {
+     alert("không phải năm nhuận");
+   }
+
+}else {
+ alert("năm nhuận");
+}
