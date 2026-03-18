0a0eb58fa387fcf35050919372494959d340d1a6
xauhoquadi123-art <xauhoquadi123@gmail.com>
2026-03-18 17:06:01 +0700

    add thuchanh4.js

--- thuchanh4.js
+++ thuchanh4.js
@@ -1,3 +1,4 @@
+function bai1() {
 let browser = prompt("Enter browser name!")
 switch(browser){
     case "Edge":
@@ -11,6 +12,20 @@ switch(browser){
         break;
         default:
            alert("We hope that this page looks ok!");
+}}
+function bai2() {
+    let a = prompt('a?','');
+    switch (a) {
+        case "0":
+            alert("0");
+            break;
+            case "1":
+                alert("1");
+                break;
+        case  "2":
+        case    "3":
+            alert("2,3");
+            break;
 
-
+    }
 }
